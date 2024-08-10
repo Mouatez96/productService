233e4930cb73c554ccc6a4a834103e7fc2e0b074
Mouatez96 <khalfaouimouatezbillah@gmail.com>
2024-08-10 18:19:31 +0200

    delete project

--- src/main/java/com/microservice/product/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.product.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.microservice.product.model.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
